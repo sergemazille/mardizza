f21cb7caafba0437726e21285d9c9585edea727b
Serge Mazille <serge.mazille@gmail.com>
2017-02-05 16:04:05 +0100

    Changed config block demo behaviour

--- src/AppBundle/Resources/public/js/mardizza/home.js
+++ src/AppBundle/Resources/public/js/mardizza/home.js
@@ -4,27 +4,33 @@
 const managerNames = ["Jon", "Sansa", "Daenerys", "Tyrion", "Ramsay", "Arya"];
 const $groupManagerInput = $(".group-manager-name");
 const $loyaltyStampsInput = $(".stamps-input");
+let changedAllowed = true;
 
+// enable/disable loyalty stamps number input on button toggling
+$('.promo-activation').on("change", function() {
+    $loyaltyStampsInput.prop("disabled", ! $loyaltyStampsInput.prop("disabled"));
+});
+
+// manager name change
 function getRandomManagerName() {
     return managerNames[Math.floor(Math.random()*managerNames.length)];
 }
 
 function changeManager() {
-    if(! $groupManagerInput.prop("disabled")) {
+    if(changedAllowed) {
         $groupManagerInput.val(getRandomManagerName());
     }
-
-    $groupManagerInput.toggleClass('refresh');
 }
+
+function buttonClickBehaviour() {
+    changedAllowed = ! $(".group-manager-name").prop("disabled");
+}
+
+$(".group-manager-button").click(changeManager);
 
 // enable/disable group manager name input on button toggling
 $('.manager-activation').on("change", function() {
     $groupManagerInput.prop("disabled", ! $groupManagerInput.prop("disabled"));
 
-    changeManager();
-});
-
-// enable/disable loyalty stamps number input on button toggling
-$('.promo-activation').on("change", function() {
-    $loyaltyStampsInput.prop("disabled", ! $loyaltyStampsInput.prop("disabled"));
+    buttonClickBehaviour();
 });
